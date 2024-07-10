054579912de2e8254c8b819efd751c48ed245ca2
shimanta <sarkershimanta@gmail.com>
2024-07-10 14:45:53 +0600

    project created

---
+++ lib/features/auth/preasentation/pages/sign_up_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:blog_app/core/theme/theme.dart';
+import 'package:blog_app/features/auth/preasentation/pages/sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.darkThemeMode,
+      home: SignUpPage(),
+    );
+  }
+}
